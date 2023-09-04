1ff67e26d0008e8ca391509a7f8529510273e6cb
Sai Sanjay Kottakota <saisanjay7660@gmail.com>
2023-09-05 02:31:56 +0530

    Again solved issue with child props

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -8,24 +8,20 @@ export default function DashboardLayout({
   children: React.ReactNode
 }) {
   return (
-    <AuthCheck
-      children={
-        <>
-          <div className="h-full relative">
-            <div
-              className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 
-            z-[80] bg-gray-900
-            "
-            >
-              <Sidebar apiLimitCount={0} isPro={false} />
-            </div>
-          </div>
-          <main className="md:pl-72">
-            <Navbar />
-            {children}
-          </main>
-        </>
-      }
-    />
-  )
+    <AuthCheck>
+      <div className="h-full relative">
+        <div
+          className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 
+        z-[80] bg-gray-900
+        "
+        >
+          <Sidebar apiLimitCount={0} isPro={false} />
+        </div>
+      </div>
+      <main className="md:pl-72">
+        <Navbar />
+        {children}
+      </main>
+    </AuthCheck>
+  );
 }
